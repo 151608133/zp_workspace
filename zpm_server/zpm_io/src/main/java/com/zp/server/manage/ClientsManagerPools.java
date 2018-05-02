8752fc1bbd4da0b4d72cd8c09223f3eeebc01e42
zhouzhe <151608133@qq.com>
2018-05-02 23:07:45 +0800

    init client manager

--- zpm_server/zpm_io/src/main/java/com/zp/server/manage/ClientsManagerPools.java
+++ zpm_server/zpm_io/src/main/java/com/zp/server/manage/ClientsManagerPools.java
@@ -1,25 +1,31 @@
 package com.zp.server.manage;
 
-import com.zp.server.impl.SessionClient;
+import com.zp.server.inter.ClientManager;
+import com.zp.server.inter.RequestHandler;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ConcurrentHashMap;
 
 
 @Component
-public class ClientsManagerPools {
+public class ClientsManagerPools implements ClientManager {
 
 
-    private ConcurrentHashMap<String,SessionClient> clientMap = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String,RequestHandler> clientMap = new ConcurrentHashMap<>();
 
 
-    /**
-     * 提交至map连接
-     * @param key
-     * @param client
-     */
-    void submit(String key, SessionClient client){
+    @Override
+    public void submit(String key, RequestHandler client) {
         clientMap.put(key,client);
     }
 
+    /**
+     * 提交链接
+     * @param key
+     * @return
+     */
+    @Override
+    public RequestHandler getClient(String key){
+        return clientMap.get(key);
+    }
 }
