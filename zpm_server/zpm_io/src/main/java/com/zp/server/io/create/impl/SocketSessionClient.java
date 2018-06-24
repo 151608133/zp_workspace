21685e71cf0c2dbc41cdc32bfa1ce3743789728e
zhouzhe <151608133@qq.com>
2018-06-24 18:33:34 +0800

    修改采集器报文读取逻辑

--- zpm_server/zpm_io/src/main/java/com/zp/server/io/create/impl/SocketSessionClient.java
+++ zpm_server/zpm_io/src/main/java/com/zp/server/io/create/impl/SocketSessionClient.java
@@ -2,6 +2,7 @@ package com.zp.server.io.create.impl;
 
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import com.zp.server.io.create.absClazz.SessionClient;
+import com.zp.server.utils.ByteUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.InputStream;
@@ -41,13 +42,9 @@ public class SocketSessionClient extends SessionClient<Socket> {
 
             b=new byte[length];
             in.read(b,0,len);
-            phoneNum = new String(Arrays.copyOfRange(b,3,14));
 
-//            for (int i=3;i<(3+11);i++){
-//                new String(b[i]);
-//                Integer index = new Integer(b[i]);
-//                phoneNum+=index;
-//            }
+            phoneNum = ByteUtils.convertHexToString(ByteUtils.bytesToHexString(Arrays.copyOfRange(b,3,14)));
+
             log.info("sim number {} , request ip {}" , phoneNum,getT().getInetAddress().getHostAddress());
         }catch (Exception e){
             log.error(e.getMessage(),e);
--- zpm_server/zpm_io/src/main/java/com/zp/server/utils/ByteUtils.java
+++ zpm_server/zpm_io/src/main/java/com/zp/server/utils/ByteUtils.java
@@ -131,4 +131,50 @@ public class ByteUtils {
         }
         return s;
     }
+
+    /**
+     * 16进制转 ascii值
+     * @param str
+     * @return
+     */
+    public static String convertStringToHex(String str) {
+
+        char[] chars = str.toCharArray();
+
+        StringBuffer hex = new StringBuffer();
+        for (int i = 0; i < chars.length; i++) {
+            hex.append(Integer.toHexString((int) chars[i]));
+        }
+
+        return hex.toString();
+    }
+
+    /**
+     * ascii值 转16进制
+     * @param hex
+     * @return
+     */
+    public static String convertHexToString(String hex) {
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+
+        // 49204c6f7665204a617661 split into two characters 49, 20, 4c...
+        for (int i = 0; i < hex.length() - 1; i += 2) {
+
+            // grab the hex in pairs
+            String output = hex.substring(i, (i + 2));
+            // convert hex to decimal
+            int decimal = Integer.parseInt(output, 16);
+            // convert the decimal to character
+            sb.append((char) decimal);
+
+            temp.append(decimal);
+        }
+
+        return sb.toString();
+    }
+
+
+
 }
